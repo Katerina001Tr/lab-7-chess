ae4ca075d1195f6fb3485d89274d6a16b8da1c28
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-10-30 14:33:58 +0500

    добавление метода ввода координат шахматной доски

--- Фт_210008_Терентьева_шахматы.py
+++ Фт_210008_Терентьева_шахматы.py
@@ -4,5 +4,23 @@
     except Exception:
         return -1
     return n
+   def Vvod(h, p):
+    n = 0
+    if h == 1:
+        while n == 0:
+            n = input(f'Введите координату от 1 до 8 по горизонтали для клетки {p}\n')
+            n = errorCheck(n)
+            if (n > 8) or (n < 1):
+                print('Ошибка ввода. Введите цифру от 1 до 8\n')
+                n=0
+        return n
+    else:
+        while n == 0:
+            n = input(f'Введите координату от 1 до 8 по вертикали для клетки {p} \n')
+            n = errorCheck(n)
+            if (n > 8) or (n < 1):
+                print('Ошибка ввода. Введите цифру от 1 до 8\n')
+                n=0
+        return n
 
             
