613d14b559d8423be42ba9ac636cda6ecc793997
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-10-30 14:44:33 +0500

    комментарии

--- Фт_210008_Терентьева_шахматы.py
+++ Фт_210008_Терентьева_шахматы.py
@@ -1,12 +1,12 @@
- def errorCheck(n):
-    try:
+ def errorCheck(n):                                                                          # обработка ошибок
+    try: 
         n = int(n)
     except Exception:
         return -1
     return n
    def Vvod(h, p):
     n = 0
-    if h == 1:
+    if h == 1:                                                                                #ввод координат для 2ух клеток
         while n == 0:
             n = input(f'Введите координату от 1 до 8 по горизонтали для клетки {p}\n')
             n = errorCheck(n)
@@ -22,7 +22,7 @@
                 print('Ошибка ввода. Введите цифру от 1 до 8\n')
                 n=0
         return n
-    def diag(n,m,i,k):
+    def diag(n,m,i,k):                                                                  #определение ходов по диагонали
         k1=k
         i1=i
         while k1!=8 and i1!=8:
@@ -55,11 +55,11 @@
             if k1==m and i1==n:
                 return 1
         return 0
-k = Vvod(1, 1)
+k = Vvod(1, 1)                                                                               #координаты полей                                                            
 i = Vvod(2, 1)
 m = Vvod(1, 2)
 n = Vvod(2, 2)
-print(f'Клетка 1: ({k},{i}); Клетка 2: ({m},{n})')
+print(f'Клетка 1: ({k},{i}); Клетка 2: ({m},{n})')                                               #вывод координат клеток
 if ((i+k+m+n) % 2 == 0):
     print ('(А) Клетки одного цвета\n')
     same=1
@@ -69,7 +69,7 @@ else:
 e=0
 
 while e == 0:
-    fig=int(input(""""Какая фигура расположена в клетке 1? Для выбора введите соответствующую цифру
+    fig=int(input(""""Какая фигура расположена в клетке 1? Для выбора введите соответствующую цифру               #ввод пользователем фигуры
               1 - ферзь
               2 - ладья
               3 - слон
@@ -83,7 +83,7 @@ while e == 0:
 res=0
 attack=0
 end=0
-if fig == 1:
+if fig == 1:                                                                                                        #стратегии для ударов фигур
     if same == 1:
         attack = diag(n,m,i,k)
     if (attack==1 or i==n or k==m):
