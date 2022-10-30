b06baa1d6d287d79db60cb6538d60cc66e307684
Katerina001Tr <93012937+Katerina001Tr@users.noreply.github.com>
2022-10-30 14:35:31 +0500

    добавление метода ходов фигур по диагонали

--- Фт_210008_Терентьева_шахматы.py
+++ Фт_210008_Терентьева_шахматы.py
@@ -22,5 +22,38 @@
                 print('Ошибка ввода. Введите цифру от 1 до 8\n')
                 n=0
         return n
+    def diag(n,m,i,k):
+        k1=k
+        i1=i
+        while k1!=8 and i1!=8:
+            i1=i1+1
+            k1=k1+1
+            if k1==m and i1==n:
+                return 1
+                
+        k1=k
+        i1=i
+        while k1!=1 and i1!=1:
+            i1=i1-1
+            k1=k1-1
+            if k1==m and i1==n:
+                return 1
+                
+        k1=k
+        i1=i
+        while k1!=8 and i1!=1:
+            i1=i1-1
+            k1=k1+1
+            if k1==m and i1==n:
+                return 1
+                
+        k1=k
+        i1=i
+        while k1!=1 and i1!=8:
+            i1=i1+1
+            k1=k1-1
+            if k1==m and i1==n:
+                return 1
+        return 0
 
             
